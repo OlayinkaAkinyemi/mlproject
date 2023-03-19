35d464d90848e6503a1ba493264f39d4d9f37fd9
OlayinkaAkinyemi <atandaodi@gmail.com>
2023-03-19 07:24:05 -0400

    committed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,16 +1,26 @@
 import pandas as pd
 import numpy as np
 import os
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
 
 class DataIngestion_config:
     train_data_path = os.path.join("artifacts", "train.csv")
     test_data_path = os.path.join("artifacts", "test.csv")
-    raw_data_path = os.path.join("artifact", "raw_data.csv")
+    raw_data_path = os.path.join("artifacts", "raw_data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestion_config()
     def initiate_data_ingestion(self):
         df = pd.read_csv("/Users/oakinyemi/Downloads/StudentsPerformance.csv")
-        return df.head()
-print(DataIngestion().initiate_data_ingestion())
+        os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
+        df.to_csv(self.ingestion_config.raw_data_path, index=False, header = True)
+        train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+        train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+        test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+        return (self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path)
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
